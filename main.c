88ef93d569f71ac3aff2def74358c0ec901ccc30
mistlure <mistlure@outlook.com>
2025-12-25 19:51:20 +0100

    First runnable version ready for testing

--- main.c
+++ main.c
@@ -14,7 +14,52 @@ int main(int argc, char **argv)
 
     CSV db;
     
+    if (loadCSVFile(args.input_file, &db) != 0)
+    {
+        return 1;
+    }
 
+    printStats(args.input_file, &db);
+
+    char buffer[1024];
+
+    ///
+
+    while (1)
+    {
+        printf("Enter command: ");
+
+        if (!fgets(buffer, sizeof(buffer), stdin))
+        {
+            break;
+        }
+
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        char *cmd = strtok(buffer, " ");
+        if (!cmd) continue;
+
+
+
+        if (strcmp(cmd, "exit") == 0)
+        {
+            printf("Saving... (%s)\n", args.output_file);
+            if (saveCSVFile(args.output_file, &db) != 0)
+            {
+                fprintf(stderr, "Error saving file!\n");
+            }
+            break;
+        }
+        else
+        {
+            printf("Undefined '%s' command.\n", cmd);
+        }
+
+        
+
+    }
+
+    ///
 
     freeCSV(&db);
     return 0;
